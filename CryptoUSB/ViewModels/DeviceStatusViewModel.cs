27e4933955d32c82cd63c1259ffc11ebb20351e5
Artur Milovidov <arturkaktus1995@gmail.com>
2023-10-30 01:02:07 +0500

    Добавление ссылки на гит

--- CryptoUSB/ViewModels/DeviceStatusViewModel.cs
+++ CryptoUSB/ViewModels/DeviceStatusViewModel.cs
@@ -1,11 +1,14 @@
-﻿using Avalonia.Interactivity;
+﻿using Avalonia.Input;
+using Avalonia.Interactivity;
 using CryptoUSB.Services;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +31,35 @@ namespace CryptoUSB.ViewModels
                         Status = "Подключен";
                     else
                         Status = "Не подключен";
+            }
+        }
+        public void GitHubPressed()
+        {
+            string url = "https://github.com/ArturKaktus/CryptoUSB";
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
             }
         }
     }
--- CryptoUSB/Views/DeviceStatus.axaml.cs
+++ CryptoUSB/Views/DeviceStatus.axaml.cs
@@ -1,15 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using CryptoUSB.ViewModels;
 
 namespace CryptoUSB.Views
 {
     public partial class DeviceStatus : UserControl
     {
+        DeviceStatusViewModel viewModel;
         public DeviceStatus()
         {
             InitializeComponent();
-            var d = new DeviceStatusViewModel();
-            this.DataContext = d;
+            viewModel = new DeviceStatusViewModel();
+            this.DataContext = viewModel;
+        }
+        public void GitHubPressed(object sender, PointerPressedEventArgs e)
+        {
+            if (viewModel != null)
+                viewModel.GitHubPressed();
         }
     }
 }
