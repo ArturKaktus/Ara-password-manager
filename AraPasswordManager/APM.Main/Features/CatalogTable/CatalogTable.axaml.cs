5980579583faba6ac991771b36d81d77592f94b2
Milovidov Artur <aomilovidov@gmail.com>
2024-08-10 23:42:08 +0500

    Удаление контекстного меню (это на будущее)

--- AraPasswordManager/APM.Main/Features/CatalogTable/CatalogTable.axaml.cs
+++ AraPasswordManager/APM.Main/Features/CatalogTable/CatalogTable.axaml.cs
@@ -18,39 +18,4 @@ public partial class CatalogTable : UserControl
         this.DataContext = new CatalogTableViewModel();
         InitializeComponent();
     }
-
-    private void TableGrid_OnPointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        var point = e.GetCurrentPoint(TableGrid);
-        if (point.Properties.IsRightButtonPressed)
-        {
-            var contextMenu = new Avalonia.Controls.ContextMenu();
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var types = assembly.GetTypes();
-            var implementingTypes = types.Where(t => t.GetInterfaces().Contains(typeof(IContextMenu)) && t.IsClass);
-            foreach (var t in implementingTypes)
-            {
-                var classInstance = Activator.CreateInstance(t);
-                if (classInstance is IContextMenu icm && icm.CanExecute(ObjectType.Record))
-                {
-                    var mi = new MenuItem() { Header = icm.Title, IsEnabled = icm.IsEnabledMenu(null), Command = icm.ExecuteCommand};
-                    contextMenu.Items.Add(mi);
-                }
-            }
-
-            // Открываем контекстное меню
-            contextMenu.Open(TableGrid);
-        }
-    }
-    public static MenuItem BuildMenuItem(string header, bool isEnable, EventHandler<RoutedEventArgs>? onClickHandler = null)
-    {
-        var item = new MenuItem { Header = header };
-        if (onClickHandler != null)
-        {
-            item.Click += onClickHandler;
-        }
-
-        return item;
-    }
 }
